5bf2d31096f7c6cd78066efce7dca90fe12a306c
Wonderzawr <beetlejuice012@gmail.com>
2025-08-31 20:11:31 +0500

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,8 @@
-fib1 = fib2 = 1
-n = int(input())
-print(fib1, fib2, end=' ')
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end=' ')
+n=int(input()) 
+a = 1 
+b = 0 
+for i in range(n): 
+    c = a 
+    a += b 
+    b = c 
+    print(b, end=" ")
